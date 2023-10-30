3b76af4d6e65735384172f6c9192150c952005c2
lichterberci <lichterberci@gmail.com>
2023-10-30 09:54:21 +0100

    Monte carlo MVP is ready, time to migrate UI

--- chessai/src/main/java/chessai/chessai/MainApplication.java
+++ chessai/src/main/java/chessai/chessai/MainApplication.java
@@ -19,7 +19,7 @@ public class MainApplication extends Application {
         String initialPosition = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 //        String initialPosition = "6k1/p2rR1p1/1p1r1p1R/3P4/4QPq1/1P6/P5PK/8 w - - 1 0";
 
-        ChessEngine engine = new MonteCarloEngine(0, 1.41, 80, 50);
+        ChessEngine engine = new MonteCarloEngine(0, 1.41, 30, 300);
 //        ChessEngine engine = new RandomEngine();
 
         gameManager.playGame(stage, PlayerType.HUMAN, PlayerType.ENGINE, null, engine, initialPosition);
