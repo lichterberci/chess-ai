7bcf963a8d50a51978fedbea14b9c44b018c2d21
lichterberci <lichterberci@gmail.com>
2023-11-19 13:17:50 +0100

    Minor changes

--- chessai/src/main/java/chessai/chessai/swing_ui/Menu.java
+++ chessai/src/main/java/chessai/chessai/swing_ui/Menu.java
@@ -1,6 +1,6 @@
 package chessai.chessai.swing_ui;
 
-import chessai.chessai.engine.MonteCarloEngine;
+import chessai.chessai.engine.MinimaxEngine;
 
 import javax.swing.*;
 import java.awt.*;
@@ -30,9 +30,9 @@ public class Menu {
 	    JButton playVsEngineBtn = new JButton("Human vs Engine");
 	    playVsEngineBtn.addActionListener(e -> {
 		    var pvpFrame = new PvEGameFrame(
-					new MonteCarloEngine(0, 1.4142, 10, 10000),
+//					new MonteCarloEngine(0, 1.4142, 10, 10000),
 //                    "8/1k6/8/8/8/8/1K1Q4/8 w - - 0 1",
-//					new MinimaxEngine(8),
+					new MinimaxEngine(8),
 					true,
 					Optional.of(10_000));
 		    pvpFrame.setVisible(true);
