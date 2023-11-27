4249c5c3775b3bdc98388c7b8ba947ef9a928975
lichterberci <lichterberci@gmail.com>
2023-11-27 21:02:15 +0100

    Added UMLs, deleted old javadocs

--- chessai/javadocs/module-search-index.js
+++
@@ -1,2 +0,0 @@
-moduleSearchIndex = [{"l": "chessai.chessai"}];
-updateSearchResults();
--- chessai/javadocs/package-search-index.js
+++
@@ -1,8 +0,0 @@
-packageSearchIndex = [{"l": "All Packages", "u": "allpackages-index.html"}, {
-    "m": "chessai.chessai",
-    "l": "chessai.chessai"
-}, {"m": "chessai.chessai", "l": "chessai.chessai.engine"}, {
-    "m": "chessai.chessai",
-    "l": "chessai.chessai.lib"
-}, {"m": "chessai.chessai", "l": "chessai.chessai.lib.pieces"}, {"m": "chessai.chessai", "l": "chessai.chessai.ui"}];
-updateSearchResults();
